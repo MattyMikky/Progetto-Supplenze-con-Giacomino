114cd86cd67f7d310f1b7bf909523d003b850c1b
Geniz-04 <mattiatudisco44@gmail.com>
2022-02-06 01:29:07 +0100

    init

--- File modificabili per prove/PySide6_prova.py
+++ File modificabili per prove/PySide6_prova.py
@@ -3,34 +3,31 @@ import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
 class MyWidget(QtWidgets.QWidget):
-  def __init__(self):
-    super().__init__()
+    def __init__(self):
+        super().__init__()
 
-    self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Ciao Mondo"]
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-    self.button = QtWidgets.QPushButton("Cliccami")
-    self.text = QtWidgets.QLabel("Hello World",
-                                  alignment=QtCore.Qt.AlignCenter)
-                          
-    
-    self.layout = QtWidgets.QVBoxLayout(self)
-    self.layout.addWidget(self.text)
-    self.layout.addWidget(self.button)
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
 
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
 
-    self.button.clicked.connect(self.magic)
+        self.button.clicked.connect(self.magic)
 
-  @QtCore.Slot()
-  def magic(self):
-    self.text.setText(random.choice(self.hello))
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
 
-if __name__ == '__main__':
-  app = QtWidgets.QApplication([])
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
 
-  
-  widget = MyWidget
-  widget.resize(800, 600)
-  widget.show()
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
 
-  sys.exit(app.exec())
+    sys.exit(app.exec())
 
