c417c30c79fdf94888f0a451ff3028c7a765b4f4
Geniz-04 <mattiatudisco44@gmail.com>
2022-03-10 01:15:48 +0100

    .

--- Progetto_Supplenze_Flask/app.py
+++ Progetto_Supplenze_Flask/app.py
@@ -1,3 +1,4 @@
+#C:\Users\matti\OneDrive\Desktop\Visual Studio Code Projects\Progetto Supplenze con Giacomino\Progetto_Supplenze_Flask\app.py
 from flask import Flask, render_template, request, flash
 
 
@@ -6,3 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+    
+    
