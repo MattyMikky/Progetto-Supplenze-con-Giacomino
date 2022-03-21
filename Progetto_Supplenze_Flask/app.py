a7fb5eff1fc2c0613952d3ad14aec65cfba6b615
Geniz-04 <mattiatudisco44@gmail.com>
2022-03-21 23:55:16 +0100

    init a caso

--- Progetto_Supplenze_Flask/app.py
+++ Progetto_Supplenze_Flask/app.py
@@ -1,12 +1,17 @@
-#C:\Users\matti\OneDrive\Desktop\Visual Studio Code Projects\Progetto Supplenze con Giacomino\Progetto_Supplenze_Flask
+# C:\Users\matti\OneDrive\Desktop\Visual_Studio_Code_Projects\Progetto_Supplenze_con_Giacomino\Progetto_Supplenze_Flask
 from flask import Flask, render_template, request, flash
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-    
- 
+# bug: cercare di risolvere il problema del terminale di VS CODE
+
+# TODO: continuare a fare il sito con bootstrap e capire come fare ad implementare il programma fatto da Giacomo
+
+# !: spaccarsi assolutamaente di video e capire come fare ahahahah
