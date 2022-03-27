0234dd3dd1fa81b6642c80f100f3a66ac609cfad
Geniz-04 <mattiatudisco44@gmail.com>
2022-03-27 17:53:54 +0200

    init

---
+++ FlaskApp/__init__.py
@@ -0,0 +1,11 @@
+#! C:\Users\matti\OneDrive\Desktop\Visual_Studio_Code_Projects\Progetto_Supplenze_con_Giacomino\FlaskApp\__init__.py
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
